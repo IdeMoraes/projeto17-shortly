1d81f69d87a051a16cf700caa22e9529e45d2ffc
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-12-23 15:08:08 -0300

    POST /signin - 2st test

--- src/controllers/signinController.js
+++ src/controllers/signinController.js
@@ -1,14 +1,24 @@
 import db from "../db.js";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
 
 export async function signin(req, res){
     const {email, password} = req.body;
     try {
-        const result = await db.query(`SELECT * FROM users WHERE email = $1 `, [email]);
-        if(result.rowCount>0){
-            console.log(result.rows);
-            res.send(result.rows);
-        }
+        const user = await db.query(`SELECT * FROM users WHERE email = $1 `, [email]);
+        if(user.rowCount===0){
+            return res.sendStatus(401);
+        };
+        if(!bcrypt.compareSync(password, user.rows[0].password)){
+            return res.sendStatus(401);  
+        };
+        const token = jwt.sign({user: user.rows[0].id}, process.env.SECRET_KEY);
+        const session = await db.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2);`, [user.rows[0].id, token]);
+        if(session.rowCount>0){
+            res.status(200).send(token);
+        };
     } catch (error) {
         console.log(error);
         res.status(500).send(`${error.name}: ${error.message}`);

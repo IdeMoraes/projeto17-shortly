a7749503432f97fd396cc2548e05fea9ffdd6b74
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-12-23 13:32:02 -0300

    POST /signup - 4th test

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,6 +1,7 @@
 import db from "../db.js"
 
 export async function insertUser(name, email, password){
-    const result = await db.query(`INSERT INTO users (name, email, password)VALUES ($1, $2, $3);`, [name, email, password]);
+    const passwordHash = bcrypt.hashSync(password, 10);
+    const result = await db.query(`INSERT INTO users (name, email, password)VALUES ($1, $2, $3);`, [name, email, passwordHash]);
     return result;
 };
